a01acde201ab7c444c5c627cfa6c6bee40f1b22e
CLGIN <xbukovinam@stuba.sk>
2024-10-16 23:38:08 +0200

    Shabby p2p thing

--- main.py
+++ main.py
@@ -1,36 +1,55 @@
 from client import Client
 from server import Server
-
+from peer import Peer
 if __name__ == '__main__':
-
-    print("Choose type - [S]erver / [C]lient")
-    print("")
-    while True:
-        program: str = input("")
-        if (program.upper() in ['S', 'C']):
-            break
-        else:
-            print("ERROR: incorect type")
-
-    ip = input("IP address D[127.0.0.1]: ")
-    listening_port = input("[S]-Listening port / [C]-Transmitting port: def[50601] ")
-    transmitting_port = input("[S]-Transmitting port / [C]-Listening port:  def[50602] ")
-    if (ip == ''):
-        ip = "127.0.0.1"
+    listening_port = input("Listening port def[50601]: ")
     if (listening_port == ''):
         listening_port = 50601
     else:
         listening_port = int(listening_port)
-    if (transmitting_port == ''):
-        transmitting_port = 50602
-    else:
-        transmitting_port = int(transmitting_port)
 
-    if (program.upper() == 'C'):
-        client = Client(ip, transmitting_port, listening_port)
-        client.launch()
-    if (program.upper() == 'S'):
-        server = Server(ip, listening_port, transmitting_port)
-        server.launch()
+    app = Peer(listening_port)
 
+    while True:
+        print("Do you want to initialize connection to a peer: [y]es I want to / [n]o I want to wait for incoming connection")
+        inp = input()
+
+        if (inp == 'y'):
+            ip = input("IP address to connect def[127.0.0.1]: ")
+            transmitting_port = input("Transmitting port def[50602]: ")
+
+            if (ip == ''):
+                ip = "127.0.0.1"
+            if (transmitting_port == ''):
+                transmitting_port = 50602
+            else:
+                transmitting_port = int(transmitting_port)
+
+            app.dest_ip = ip
+            app.port_transmit = transmitting_port
+
+
+            if (app.conn_initializer()):
+                break
+            else:
+                print("Couldn't establish connection")
+
+        elif (inp == 'n'):
+            transmitting_port = input("Transmitting port def[50602]: ")
+
+            if (transmitting_port == ''):
+                transmitting_port = 50602
+            else:
+                transmitting_port = int(transmitting_port)
+
+            app.port_transmit = transmitting_port
+
+            if (app.conn_listener(listening_port)):
+                break
+            else:
+                print("Couldn't establish connection")
+        else:
+            print("Invalid input")
+
+    app.communicate()
 
