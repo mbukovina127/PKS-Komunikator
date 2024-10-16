a419f73bc5fa06b17584497143c9bd4178f70f6c
CLGIN <xbukovinam@stuba.sk>
2024-10-17 00:20:54 +0200

    p2p more fixes

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ if __name__ == '__main__':
         listening_port = int(listening_port)
 
     app = Peer(listening_port)
+    transmitting_port = input("Transmitting port def[50602]: ")
+
+    if (transmitting_port == ''):
+        transmitting_port = 50602
+    else:
+        transmitting_port = int(transmitting_port)
 
     while True:
         print("Do you want to initialize connection to a peer: [y]es I want to / [n]o I want to wait for incoming connection")
@@ -16,14 +22,10 @@ if __name__ == '__main__':
 
         if (inp == 'y'):
             ip = input("IP address to connect def[127.0.0.1]: ")
-            transmitting_port = input("Transmitting port def[50602]: ")
+
 
             if (ip == ''):
                 ip = "127.0.0.1"
-            if (transmitting_port == ''):
-                transmitting_port = 50602
-            else:
-                transmitting_port = int(transmitting_port)
 
             app.dest_ip = ip
             app.port_transmit = transmitting_port
@@ -35,16 +37,10 @@ if __name__ == '__main__':
                 print("Couldn't establish connection")
 
         elif (inp == 'n'):
-            transmitting_port = input("Transmitting port def[50602]: ")
-
-            if (transmitting_port == ''):
-                transmitting_port = 50602
-            else:
-                transmitting_port = int(transmitting_port)
 
             app.port_transmit = transmitting_port
 
-            if (app.conn_listener(listening_port)):
+            if (app.conn_listener()):
                 break
             else:
                 print("Couldn't establish connection")
